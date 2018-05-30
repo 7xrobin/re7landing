9feafc47e349a1bbf85628a21dc7cfc37a99f421
Bharata <fernando.rafael.ferreira@gmail.com>
2018-05-30 12:29:42 -0400

    Adds a form to register new contacts.

--- contact.php
+++ contact.php
@@ -10,13 +10,16 @@
         $success = file_put_contents($CONTACTS, json_encode($contact) . PHP_EOL, FILE_APPEND);
         if ($success) {
             http_response_code(200);
+            echo("Contato registrado.");
         } else {
             http_response_code(500);
+            echo("Falha ao registrar contato.");
         }
 
     } else {
         // Bad Request, name and e-mail are required.
         http_response_code(400);
+        echo("O nome e o e-mail são obrigatórios.");
     }
 
 ?>
